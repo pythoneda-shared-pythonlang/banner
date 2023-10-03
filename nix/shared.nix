50a49eeb6f94ecaebf131c22db42ad4c0af613be
rydnr <github@acm-sl.org>
2023-10-03 12:49:17 +0200

    Added optional processPythonpath and rootFolder parameters

--- nix/shared.nix
+++ nix/shared.nix
@@ -19,7 +19,7 @@
 rec {
   shellHook-for = { archRole, layer, nixpkgsRelease, org, package, python
     , pythoneda-shared-pythoneda-domain, pythoneda-shared-pythoneda-banner, repo
-    , space }:
+    , space, processPythonpath, rootFolder }:
     let
       pythonVersionParts = builtins.splitVersion python.version;
       pythonMajorVersion = builtins.head pythonVersionParts;
@@ -44,35 +44,15 @@ rec {
       export PS1="$($_PYTHONEDA_BANNER/bin/ps1.sh -o $_PYTHONEDA_ORG -r $_PYTHONEDA_REPO -t $_PYTHONEDA_PACKAGE_TAG -s $_PYTHONEDA_SPACE -a $_PYTHONEDA_ARCH_ROLE -l $_PYTHONEDA_LAYER -p $_PYTHONEDA_PYTHON_VERSION -n $_PYTHONEDA_NIXPKGS_RELEASE -D $_PYTHONEDA_DEPS -d $_PYTHONEDA_PYTHONEDA_DEPS)";
       $_PYTHONEDA_BANNER/bin/banner.sh -o $_PYTHONEDA_ORG -r $_PYTHONEDA_REPO -t $_PYTHONEDA_PACKAGE_TAG -s $_PYTHONEDA_SPACE -a $_PYTHONEDA_ARCH_ROLE -l $_PYTHONEDA_LAYER -p $_PYTHONEDA_PYTHON_VERSION -n $_PYTHONEDA_NIXPKGS_RELEASE -D $_PYTHONEDA_DEPS -d $_PYTHONEDA_PYTHONEDA_DEPS
       export _PYTHONEDA_PYTHONPATH_OLD="$PYTHONPATH";
-      export PYTHONPATH="$(python $_PYTHONEDA/dist/scripts/process_pythonpath.py development)";
-    '';
-  raw-shellHook-for = { archRole, layer, nixpkgsRelease, org, package, python
-    , pythoneda-shared-pythoneda-domain, pythoneda-shared-pythoneda-banner, repo
-    , space }:
-    let
-      pythonVersionParts = builtins.splitVersion python.version;
-      pythonMajorVersion = builtins.head pythonVersionParts;
-      pythonMajorMinorVersion =
-        "${pythonMajorVersion}.${builtins.elemAt pythonVersionParts 1}";
-    in ''
-      export _PYTHONEDA_PACKAGE_NAME="${package.pname}";
-      export _PYTHONEDA_PACKAGE_VERSION="${package.version}";
-      export _PYTHONEDA_PYTHON_NAME="${python.name}";
-      export _PYTHONEDA_PYTHON_VERSION="${pythonMajorMinorVersion}";
-      export _PYTHONEDA_NIXPKGS_RELEASE="${nixpkgsRelease}";
-      export _PYTHONEDA="${pythoneda-shared-pythoneda-domain}";
-      export _PYTHONEDA_BANNER="${pythoneda-shared-pythoneda-banner}";
-      export _PYTHONEDA_ORG="${org}";
-      export _PYTHONEDA_ARCH_ROLE="${archRole}";
-      export _PYTHONEDA_LAYER="${layer}";
-      export _PYTHONEDA_REPO="${repo}";
-      export _PYTHONEDA_SPACE="${space}";
-      export _PYTHONEDA_PACKAGE_TAG="${package.version}";
-      export _PYTHONEDA_DEPS="$(echo $PYTHONPATH | sed 's : \n g' | wc -l)"
-      export _PYTHONEDA_PYTHONEDA_DEPS="$(echo $PYTHONPATH | sed 's : \n g' | grep 'pythoneda' | wc -l)"
-      export PS1="$($_PYTHONEDA_BANNER/bin/ps1.sh -o $_PYTHONEDA_ORG -r $_PYTHONEDA_REPO -t $_PYTHONEDA_PACKAGE_TAG -s $_PYTHONEDA_SPACE -a $_PYTHONEDA_ARCH_ROLE -l $_PYTHONEDA_LAYER -p $_PYTHONEDA_PYTHON_VERSION -n $_PYTHONEDA_NIXPKGS_RELEASE -D $_PYTHONEDA_DEPS -d $_PYTHONEDA_PYTHONEDA_DEPS)";
-      $_PYTHONEDA_BANNER/bin/banner.sh -o $_PYTHONEDA_ORG -r $_PYTHONEDA_REPO -t $_PYTHONEDA_PACKAGE_TAG -s $_PYTHONEDA_SPACE -a $_PYTHONEDA_ARCH_ROLE -l $_PYTHONEDA_LAYER -p $_PYTHONEDA_PYTHON_VERSION -n $_PYTHONEDA_NIXPKGS_RELEASE -D $_PYTHONEDA_DEPS -d $_PYTHONEDA_PYTHONEDA_DEPS
-      export _PYTHONEDA_PYTHONPATH_OLD="$PYTHONPATH";
+      export _PYTHONEDA_PROCESS_PYTHONPATH="${processPythonpath}";
+      export _PYTHONEDA_ROOT_FOLDER="${rootFolder}";
+      if [[ $_PYTHONEDA_PROCESS_PYTHONPATH == "true" ]]; then
+        if [[ $_PYTHONEDA_ROOT_FOLDER == "" ]]; then
+          export PYTHONPATH="$(python $_PYTHONEDA/dist/scripts/process_pythonpath.py development)";
+        else
+          export PYTHONPATH="$(python $_PYTHONEDA/dist/scripts/process_pythonpath.py -r $_PYTHONEDA_ROOT_FOLDER development)";
+        fi
+      fi
     '';
   devShell-for = { archRole, layer, nixpkgsRelease, org, package, pkgs, python
     , pythoneda-shared-pythoneda-domain, pythoneda-shared-pythoneda-banner, repo
@@ -80,17 +60,6 @@ rec {
     pkgs.mkShell {
       buildInputs = [ package pythoneda-shared-pythoneda-banner ];
       shellHook = shellHook-for {
-        inherit archRole layer nixpkgsRelease org package python
-          pythoneda-shared-pythoneda-domain pythoneda-shared-pythoneda-banner
-          repo space;
-      };
-    };
-  raw-devShell-for = { archRole, layer, nixpkgsRelease, org, package, pkgs
-    , python, pythoneda-shared-pythoneda-domain
-    , pythoneda-shared-pythoneda-banner, repo, space }:
-    pkgs.mkShell {
-      buildInputs = [ package pythoneda-shared-pythoneda-banner ];
-      shellHook = raw-shellHook-for {
         inherit archRole layer nixpkgsRelease org package python
           pythoneda-shared-pythoneda-domain pythoneda-shared-pythoneda-banner
           repo space;
