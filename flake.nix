db254a4c04e92b6ed9ac50b511baf9974cd4b588
rydnr <github@acm-sl.org>
2023-11-13 15:56:18 +0100

    Added PYTHONEDA_EXTRA_NAMESPACES configuration

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
         org = "pythoneda-shared-pythoneda";
         repo = "banner";
         pname = "${org}-${repo}";
-        version = "0.0.13";
+        version = "0.0.14";
         pkgs = import nixos { inherit system; };
         pythonpackage = "pythoneda.banner";
         package = builtins.replaceStrings [ "." ] [ "/" ] pythonpackage;
