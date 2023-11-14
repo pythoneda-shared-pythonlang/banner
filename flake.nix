8760d14000e0b2cf11585a25e68a4fa11cb0422c
rydnr <github@acm-sl.org>
2023-11-14 13:04:23 +0100

    Added dry-wit dependency to nix/shell.nix

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
         org = "pythoneda-shared-pythoneda";
         repo = "banner";
         pname = "${org}-${repo}";
-        version = "0.0.15";
+        version = "0.0.16";
         pkgs = import nixos { inherit system; };
         pythonpackage = "pythoneda.banner";
         package = builtins.replaceStrings [ "." ] [ "/" ] pythonpackage;
