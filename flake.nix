404b3bbf32b4ec1242b349645c1b69c48eaba2fe
rydnr <github@acm-sl.org>
2023-11-14 07:38:36 +0100

    Updated version

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
         org = "pythoneda-shared-pythoneda";
         repo = "banner";
         pname = "${org}-${repo}";
-        version = "0.0.14";
+        version = "0.0.15";
         pkgs = import nixos { inherit system; };
         pythonpackage = "pythoneda.banner";
         package = builtins.replaceStrings [ "." ] [ "/" ] pythonpackage;
