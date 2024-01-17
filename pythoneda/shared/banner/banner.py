e08ad747978516fb0194c89b478711425986517e
rydnr <github@acm-sl.org>
2024-01-17 19:03:44 +0100

    Using absolute imports to avoid import erro
    ImportError: attempted relative import with no known parent

--- pythoneda/shared/banner/banner.py
+++ pythoneda/shared/banner/banner.py
@@ -20,7 +20,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-from .metadata import Metadata
+from pythoneda.shared.banner import Metadata
 from typing import List
 
 
